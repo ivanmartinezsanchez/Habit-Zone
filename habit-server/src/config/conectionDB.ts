a4df21cdfbc5c797042361b945bc79f0d2e74f07
ivanmartinezsanchez <poyovk@gmail.com>
2025-04-25 15:40:33 +0200

    Fix connectionDB AND .env

--- habit-server/src/config/conectionDB.ts
+++ habit-server/src/config/conectionDB.ts
@@ -1,22 +1,20 @@
 import mysql from 'mysql';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config();    
 
 export const habits_db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  multipleStatements: true, // opcional si usas procedimientos almacenados
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 habits_db.connect((err) => {
-  if (err) {
-    console.error('❌ Error connecting to DB:', err.message);
-    process.exit(1); // Detiene la app si falla la conexión
-  } else {
+    if (err) {
+        console.error('❌ Error connecting to DB:', err.message);
+        return;
+    }
     console.log('✅ Connected to the HABITS database!');
-  }
 });
